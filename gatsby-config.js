3dc4a5f5f20c97eef3880803c43735195d6b4dd3
BoyWithSilverWings <agney@outlook.in>
2018-12-29 08:28:00 +0530

    Install necessary package

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Aviato - Careers Page`,
+    description: `Create an Aviato Careers Page`,
+    author: `@agneymenon`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,11 +24,10 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-svg`,
   ],
 }
