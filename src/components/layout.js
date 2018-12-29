43309a141528147a3b30c6f38c7e5adf38db363b
BoyWithSilverWings <agney@outlook.in>
2018-12-29 08:22:19 +0530

    Remove placeholder code

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,9 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
 
-import Header from './header'
-import './layout.css'
-
 const Layout = ({ children }) => (
   <StaticQuery
     query={graphql`
@@ -18,15 +15,7 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
+        <div>
           {children}
           <footer>
             © 2018, Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
